ebc6e54598622ef5afadd85b50bbd9d909cb52b9
Jace <jddelfin018@gmail.com>
2026-05-28 23:56:10 +0800

    added login required in supplies API

--- routes/supplies.py
+++ routes/supplies.py
@@ -14,30 +14,42 @@ supplies_bp = Blueprint('supplies', __name__)
 
 #List all supplies with supplier details
 @supplies_bp.route('/', methods = ['GET'])
+@logged_in_required
+@role_required('admin')
 def list_supplies():
     return getSupplies()
 
 #Get supply details
 @supplies_bp.route('/details', methods = ['GET'])
+@logged_in_required
+@role_required('admin')
 def get_supply_details():
     return getSuppliesDetail()
 
 #Create supply
 @supplies_bp.route('/', methods = ['POST'])
+@logged_in_required
+@role_required('admin')
 def add_supply():
     return createSupplies()
 
 #Update supply
 @supplies_bp.route('/<int:supply_id>', methods = ['PUT'])
+@logged_in_required
+@role_required('admin')
 def update_supply(supply_id):
     return updateSupplies(supply_id)
 
 #Delete supply
 @supplies_bp.route('/<int:supply_id>', methods = ['DELETE'])
+@logged_in_required
+@role_required('admin')
 def delete_supply(supply_id):
     return deleteSupplies(supply_id)
 
 #Get supplies that are below reorder level
 @supplies_bp.route('/low-stock', methods = ['GET'])
+@logged_in_required
+@role_required('admin')
 def low_stock_supplies():
     return getLowStockSupplies()
